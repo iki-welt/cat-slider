6b5dcaeed1eaa0b53d0abd9c256b267b293b8044
iki_welt <iki.welt@ya.ru>
2023-12-04 20:36:18 +0300

    Попытка оформить состояния - не получилось

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const slider = document.querySelector(".slider__inp");
 const img = document.querySelector(".slide__before-wrapper");
 const thumb = document.querySelector(".slider__thumb");
+
 slider.oninput = ()=>{
 let sliderVal = slider.value;
 thumb.style.left = sliderVal + "%";
